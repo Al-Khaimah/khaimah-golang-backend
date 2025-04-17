b08364494ca8a4a14c71a51bbaadaf356cdaba97
ZiyadRW <zeadAlrouasheed.com>
2025-04-17 14:23:57 +0300

    add admin

--- internal/modules/users/models/user.go
+++ internal/modules/users/models/user.go
@@ -4,13 +4,15 @@ import (
 	"github.com/Al-Khaimah/khaimah-golang-backend/internal/base"
 	categories "github.com/Al-Khaimah/khaimah-golang-backend/internal/modules/categories/models"
 	podcasts "github.com/Al-Khaimah/khaimah-golang-backend/internal/modules/podcasts/models"
+	users "github.com/Al-Khaimah/khaimah-golang-backend/internal/modules/users/enums"
 )
 
 type User struct {
 	base.Model
-	FirstName string `gorm:"type:varchar(100)" json:"first_name"`
-	LastName  string `gorm:"type:varchar(100)" json:"last_name"`
-	Email     string `gorm:"type:varchar(255);uniqueIndex" json:"email"`
+	FirstName string         `gorm:"type:varchar(100)" json:"first_name"`
+	LastName  string         `gorm:"type:varchar(100)" json:"last_name"`
+	UserType  users.UserType `gorm:"type:varchar(20);default:'free'" json:"user_type"`
+	Email     string         `gorm:"type:varchar(255);uniqueIndex" json:"email"`
 
 	Categories []categories.Category `gorm:"many2many:user_categories" json:"categories"`
 	Bookmarks  []podcasts.Podcast    `gorm:"many2many:user_bookmarks" json:"bookmarks,omitempty"`
